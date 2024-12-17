6c9bdd6a023681d66586dc8bfebc6fe011c5b5bf
“imtiazins” <imtiaz3996@gmail.com>
2024-12-17 15:20:37 +0600

    display phones

--- js/phone.js
+++ js/phone.js
@@ -11,8 +11,26 @@ const displayPhones = phones => {
     phones.forEach(phone=>{
         console.log(phone)
 
+    const phoneContainer = document.getElementById('phone-container')
+
         // 1 create a div 
-        
+        const phoneCard = document.createElement(`div`);
+        phoneCard.classList = `card bg-gray-100 image-full w-96 shadow-xl`
+        phoneCard.innerHTML=`
+         <figure>
+                <img
+                  src="${phone.image}" />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                  <button class="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+        `;
+        phoneContainer.appendChild(phoneCard)
+
     })
 }
 
